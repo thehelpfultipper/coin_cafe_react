5831dc16d5f1752f11b6c296a88cdcdaf333ca6c
THT <thehelpfultipper@gmail.com>
2023-10-22 18:02:09 -0700

    Adjust container resizing

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,6 +7,10 @@ const Header = () => {
     return(
         <div className={s.container}>
             <Nav />
+            <div className={s.content}>
+                <h1>Welcome to Coin Cafe, where every cup is a treasured moment.</h1>
+                <p>Step inside and experience the currency of great taste, where we brew not just coffee but connections. Join us for a journey through flavors that enrich your senses, and savor the moments that make life priceless.</p>
+            </div>
         </div>
     )
 }
