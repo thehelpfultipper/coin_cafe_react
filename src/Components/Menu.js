fa3729ccc8905c3320371338eaf16145e1e5f1e0
THT <thehelpfultipper@gmail.com>
2023-11-04 14:18:19 -0700

    Adjust for new data source

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -4,41 +4,30 @@ import MenuItem from './MenuItem';
 
 import s from './Menu.module.scss';
 
-let apiUrls = [
-    'https://api.sampleapis.com/coffee/hot',
-    'https://api.sampleapis.com/coffee/iced'
-];
-
-const generateRandomPrice = () => {
-    let num = Math.floor(Math.random() * 890 + 100) // number between 100 - 890
-    let price = (num / 100).toFixed(2);
-
-    return price;
-}
+let URL = 'https://thehelpfultipper.github.io/coin_cafe_react/coffee.json';
 
 const Menu = () => {
     let [data, setData] = useState([]);
 
     const getCoffeeData = async () => {
-        let allData = [];
-        
-        const responses = await Promise.all(apiUrls.map(url => fetch(url)));
-        const data = await Promise.all(responses.map( resp => resp.json()));
-
-        data.forEach( type => allData.push(...type));
-        
-        setData(allData);
+        try {
+            const resp = await fetch(URL);
+            const data = await resp.json();
+           
+            setData(data);
+        } catch (e) {
+            console.log(e)
+        }
     }
 
-    useEffect( () => {
+    useEffect(() => {
         getCoffeeData(); // desc, id, img, title 
     }, []);
-    
+
     return (
         <div className={s.container}>
-            {data.map( (item, i) => {
-                let newItem = {...item, price: generateRandomPrice(), count: 1}
-                return <MenuItem data={newItem} id={i} key={i} />
+            {data.map((item, i) => {
+                return <MenuItem data={item} id={i} key={i} />
             })}
         </div>
     )
